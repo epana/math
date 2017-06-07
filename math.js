4bf8e478e23e70b9874372751c664df069bcab6a
Yvonne Kim <ykim@ykim-mbp.corp.shutterfly.com>
2017-06-06 19:04:43 -0700

    replace odd list with 50

--- math.js
+++ math.js
@@ -1,3 +1,18 @@
 var multiplyTwo = function(numberOne, numberTwo) {
 		return numberOne * numberTwo
 }
+
+var list = [1,3,5,7,9]
+
+console.log(list[0])
+list[0] = 50
+console.log(list)
+
+
+
+/*
+
+for (var i = 0; i < list.length; i++) {
+
+}
+*/
